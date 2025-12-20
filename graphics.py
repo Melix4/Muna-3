03f29f627c05914cf6d2ba8c0192a7b689782c2d
Alexander Melnikov <Msan90546@mail.ru>
2025-12-20 22:31:21 +0300

    Changed model for 2D variant

--- graphics.py
+++ graphics.py
@@ -7,64 +7,62 @@ if not os.path.isdir("Graphics"):  # папка для хранения граф
 
 # Построение графиков функции
 # Получение всех необходимых массивов
-massArray, accelerationArray, heightArray, speedArray, gravityForceArray, time = main.calc()
+timeArray, massArray, velocityArray, heightArray, thrustArray, gravityArray, dragArray, RoArray = main.calc()
 
-massArrayKSP, altitudeArrayKSP = main.data_from_ksp()
+#massArrayKSP, altitudeArrayKSP = main.data_from_ksp()
 
 # График №1: Зависимость массы ракеты от времени
 massGraph = plt.figure("Масса", figsize=(10, 5), dpi=150)
-plt.plot(time, massArray, 'g')
+plt.plot(timeArray, massArray, 'g')
 plt.title("Масса от времени")
 plt.xlabel('Время, с')
 plt.ylabel('Масса, кг')
 massGraph.savefig("Graphics/massGraph.png")
 
-# График №2: Зависимость ускорения ракеты от времени
-accelerationGraph = plt.figure("Ускорение", figsize=(10, 5), dpi=150)
-plt.plot(time, accelerationArray, 'Orange')
-plt.title("Ускорение от времени")
-plt.xlabel('Время, с')
-plt.ylabel('Ускорение, м/с^2')
-accelerationGraph.savefig("Graphics/accelerationGraph.png")
-
-# График №3: Зависимость высоты ракеты от времени
+# График №2: Зависимость высоты ракеты от времени
 heightGraph = plt.figure("Высота", figsize=(10, 5), dpi=150)
-plt.plot(time, heightArray, 'r')
+plt.plot(timeArray, heightArray, 'r')
 plt.title("Высота от времени")
 plt.xlabel('Время, с')
 plt.ylabel('Высота, м')
 heightGraph.savefig("Graphics/heightGraph.png")
 
-# График №4: Зависимость скорости ракеты от времени
+# График №3: Зависимость скорости ракеты от времени
 speedGraph = plt.figure("Скорость", figsize=(10, 5), dpi=150)
-plt.plot(time, speedArray, 'r')
+plt.plot(timeArray, velocityArray, 'b')
 plt.title("Скорость от времени")
 plt.xlabel('Время, с')
 plt.ylabel('Скорость, м/с')
 speedGraph.savefig("Graphics/speedGraph.png")
 
-# График №5: Зависимость силы гравитации от высоты
+# График №4: Зависимость силы гравитации от высоты
 gravityForceGraph = plt.figure("Гравитация", figsize=(10, 5), dpi=150)
-plt.plot(heightArray[2:], gravityForceArray[2:], 'r')
+plt.plot(heightArray, gravityArray, 'g')
 plt.title("Сила гравитации от высоты")
 plt.xlabel('Высота, м')
 plt.ylabel('Сила гравитационного притяжения, Н')
 gravityForceGraph.savefig("Graphics/gravityForceGraph.png")
 
-# Сравнение графиков масс
-compMass = plt.figure("Сравнение масс", figsize=(10, 5), dpi=150)
-plt.plot(time, massArray, label='Данные с физ.модели')
-plt.plot(time, massArrayKSP, label='Данные c ksp')
+# График №5: Зависимость силы сопротивления от времени
+dragGraph = plt.figure("Сопротивление", figsize=(10, 5), dpi=150)
+plt.plot(timeArray, dragArray, 'r')
+plt.title("Сила сопротивления среды от времени")
 plt.xlabel('Время, с')
-plt.ylabel('Масса, кг')
-plt.title('Сравнение масс')
-compMass.savefig("Graphics/comparisonMass.png")
+plt.ylabel('Сила сопротивления воздуха, Н')
+dragGraph.savefig("Graphics/dragGraph.png")
 
-# Сравнение графиков высот
-compHeight = plt.figure("Сравнение высот", figsize=(10, 5), dpi=150)
-plt.plot(time, heightArray, label='Данные с физ.модели')
-plt.plot(time, altitudeArrayKSP, label='Данные с ksp')
+# График №6: Зависимость плотности от высоты
+RoGraph = plt.figure("Плотность", figsize=(10, 5), dpi=150)
+plt.plot(heightArray, RoArray, 'b')
+plt.title("Плотность среды от высоты")
+plt.xlabel('Высота, м')
+plt.ylabel('Плотность среды, Па')
+RoGraph.savefig("Graphics/RoGraph.png")
+
+# График №7: Зависимость силы тяги от времени
+thrustGraph = plt.figure("Тяга", figsize=(10, 5), dpi=150)
+plt.plot(timeArray, thrustArray, 'g')
+plt.title("Тяга от времени")
 plt.xlabel('Время, с')
-plt.ylabel('Высота, м')
-plt.title('Сравнение высот')
-compHeight.savefig("Graphics/comparisonHeight.png")
+plt.ylabel('Сила тяги среды, H')
+thrustGraph.savefig("Graphics/thrustGraph.png")
--- main.py
+++ main.py
@@ -4,11 +4,24 @@ import pandas as pd
 import openpyxl
 
 
+def vector_length(vec):
+    return (vec[0] ** 2 + vec[1] ** 2) ** 0.5
+
+
 # Возвращает номер ступени в зависимости от времени
 def stage(t):
     if 0 <= t <= 25:
         return 1
     return 2
+
+
+def angle(h):
+    h = h - constants.Kerbin_Radius
+    if h <= 10000:
+        return 90
+    elif h >= 20000:
+        return 45
+    return 90 - ((h - 10000) / (20000 - 10000) * 45)
 
 
 # Функция рассчета коэффициента расхода топлива
@@ -28,9 +41,9 @@ def current_rocket_mass(t):
 
 
 # Функция рассчета гравитационного воздействия на ракету
-def gravity_force(d):
-    R = constants.Kerbin_Radius
-    return constants.Rocket_Mass * constants.g0 * (R ** 2 / (R + d) ** 2)
+def gravity_force(d, m0):
+    d += constants.Kerbin_Radius
+    return constants.Kerbin_Mass * m0 * constants.Gravity_Parametr / (d ** 2)
 
 
 # Функция, возвращающая реактивную тягу ракеты
@@ -40,52 +53,117 @@ def reactive_thrust(t):
     return constants.Isp[s - 1] * constants.g0 * fuel_consumption(t)
 
 
-# Функция, рассчитывающая характеристическую скорость ракеты по формуле Циолковского
-def deltaV(t):
-    s = stage(t)
-    return constants.Isp[s - 1] * constants.g0 * math.log(constants.Rocket_Mass / current_rocket_mass(t))
+# сила сопротивления среды (воздуха) в Ньютонах
+def drag_force(velocity, Ro):
+    # return (constants.Cf * Ro * velocity ** 2 * constants.S) / 2
+    return 0
 
 
-# Функция рассчета ускорения ракеты в заданный момент времени t
-def acceleration(heightArray, t):
-    height = heightArray[-1]
-    return (reactive_thrust(t) - gravity_force(height)) / current_rocket_mass(t)
+# атмосферное давление от высоты
+def pressure(h):
+    return constants.Pressure * math.exp(-(constants.Molar_Mass * constants.g0 * h) / (constants.R * constants.T))
+
+
+# ускорение, м/с^2
+def acceleration(ForceOfThrust, ForceOfGravity, ForceOfAirResistance, Angle, mass):
+    return ((abs(ForceOfThrust * math.cos(math.radians(Angle)) + ForceOfGravity[0] + ForceOfAirResistance[0])) / mass,
+            abs((ForceOfThrust * math.sin(math.radians(Angle)) + ForceOfGravity[1] + ForceOfAirResistance[1]) / mass))
+
+
+def zero_state():
+    return State(current_rocket_mass(0), 0, constants.Kerbin_Radius, 0, 0, 0, 0, angle(0))
+
+
+class State:
+    def __init__(self, m, x_pos, y_pos, vx, vy, ax, ay, angle):
+        self.m = m
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.vx = vx
+        self.vy = vy
+        self.ax = ax
+        self.ay = ay
+        self.angle = angle
+
+    def get_next_state(self, time):
+        m0 = self.m
+        h = self.y_pos
+        Ro = pressure(h)
+        v = vector_length((self.vx, self.vy))
+
+        if v != 0:
+            vx_direction = self.vx / v
+            vy_direction = self.vy / v
+        else:
+            vx_direction = math.cos(math.radians(self.angle))
+            vy_direction = math.sin(math.radians(self.angle))
+
+        F_thrust = reactive_thrust(time)
+        F_gravity = gravity_force(h, m0)
+        F_drag = drag_force(v, Ro)
+        phi = math.radians(angle(h))
+
+        F_thrust_x = F_thrust * math.cos(phi)
+        F_thrust_y = F_thrust * math.sin(phi)
+
+        F_drag_x = -F_drag * vx_direction
+        F_drag_y = -F_drag * vy_direction
+
+        F_gravity_x = 0
+        F_gravity_y = -F_gravity
+
+        F_total_x = F_thrust_x + F_drag_x + F_gravity_x
+        F_total_y = F_thrust_y + F_drag_y + F_gravity_y
+
+        # ускорения(второй закон Ньютона)
+        ax = F_total_x / m0
+        ay = F_total_y / m0
+
+        self.vx += ax
+        self.vy += ay
+        state_arr = [current_rocket_mass(time), self.x_pos + self.vx, self.y_pos + self.vy,
+                     self.vx, self.vy, ax, ay, angle(self.y_pos)]
+        return State(*state_arr)
+
+    def get_array_state(self):
+        return [self.m, self.x_pos, self.y_pos, self.vx, self.vy, self.ax, self.ay, self.angle]
 
 
 # Функция, проводящая моделирование процесса полета и запись значений в списки
 def calc():
-    timeArray = [i for i in range(constants.t + 1)]  # Список временных значений
-    massArray = [current_rocket_mass(time) for time in timeArray]  # Список масс ракеты для каждого времени
-    # speedArray = [deltaV(time) for time in timeArray]  # Список значений скорости ракеты для каждого времени
-    speedArray = [0]
-    # Заполнение значениями списка высот ракеты относительно Кербина для каждого времени
-    heightArray = [0]
-
-    accelerationArray = [acceleration(heightArray, 0)]
-    for time in timeArray[1:]:
-        v = speedArray[-1]
-        a = acceleration(heightArray, time)
-        v += a
-        currentHeight = heightArray[-1] + v
-        heightArray.append(currentHeight)
-        accelerationArray.append(a)
-        speedArray.append(v)
-
-    gravityForceArray = [gravity_force(d) for d in heightArray]  # Список значений силы гравитации для каждого времени
-    # Возврат списков значений
-    return massArray, accelerationArray, heightArray, speedArray, gravityForceArray, timeArray
+    time = 0
+    state = zero_state()
+    timeArray, massArray, velocityArray, heightArray, thrustArray, gravityArray, dragArray, RoArray = (
+        [], [], [], [], [], [], [], [])
+    while time <= constants.t:
+        state = State(*state.get_array_state())
+        state = state.get_next_state(time)
+        print(*state.get_array_state())
+        timeArray.append(time)
+        massArray.append(state.get_array_state()[0])
+        velocityArray.append(state.get_array_state()[4])
+        heightArray.append(state.get_array_state()[2] - constants.Kerbin_Radius)
+        time += 1
+    thrustArray = [reactive_thrust(t) for t in timeArray]
+    gravityArray = [gravity_force(heightArray[t], massArray[t]) for t in range(constants.t + 1)]
+    RoArray = [pressure(h) for h in heightArray]
+    dragArray = [drag_force(velocityArray[t], RoArray[t]) for t in range(len(velocityArray))]
+    return timeArray, massArray, velocityArray, heightArray, thrustArray, gravityArray, dragArray, RoArray
 
 
 # Функция записи данных в Excel-файл
 def write_to_xls():
     # Получение списков данных и создание словаря dataDict, содержащего заголовки
-    massArray, accelerationArray, heightArray, speedArray, gravityForceArray, timeArray = calc()
+    timeArray, massArray, velocityArray, heightArray, thrustArray, gravityArray, dragArray, RoArray = calc()
     dataDict = {
         'Время': timeArray,
         'Масса': massArray,
-        'Ускорение': accelerationArray,
-        'Скорость': speedArray,
-        'Высота': heightArray
+        'Скорость': velocityArray,
+        'Высота': heightArray,
+        'Тяга': thrustArray,
+        'Сопротивление среды': dragArray,
+        'Плотность среды': RoArray,
+        'Гравитация': gravityArray
     }
     # создание объекта DataFrame модуля Pandas
     df = pd.DataFrame(dataDict)
@@ -103,7 +181,7 @@ def data_from_ksp():
     for x in data:
         # разбиваем каждую строку по запятой, выбираем необходимые элементы
         x = x.split(',')
-        time = int(float(x[0]))
+        time = int(float(x[0]) * 10)
         mass = float(x[1])
         altitude = float(x[2])
 
